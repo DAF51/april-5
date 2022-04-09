d0b21b00d9c97ec4a953907ee218cc4bada7f723
Daniel Figuereo <danielafig51@gmail.com>
2022-04-09 14:53:00 -0400

    Created the corporate speech generator

--- api projects/Corporate bs generator/js/main.js
+++ api projects/Corporate bs generator/js/main.js
@@ -1,1 +1,16 @@
+const corpButton = document.querySelector("button")
+const speechSpace = document.querySelector(".dialogue")
+corpButton.addEventListener("click", corpSpeak)
 
+
+function corpSpeak(){
+  let para = document.createElement("p")
+  let url = "https://corporatebs-generator.sameerkumar.website/"
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      para = para.innerText = `${data.phrase}`
+      speechSpace.innerText = para
+    })
+}
